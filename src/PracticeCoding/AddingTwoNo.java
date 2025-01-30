48e01a729718b36067b1393e7563d4539bfabc1f
Admin <truptisawardekar1510@gmail.com>
2025-01-30 13:41:39 +0530

    Practice 2

---
+++ src/PracticeCoding/AddingTwoNo.java
@@ -0,0 +1,15 @@
+package PracticeCoding;
+
+public class AddingTwoNo {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c= a+b;
+
+        System.out.println(c);
+
+
+        System.out.println(a+b);
+    }
+
+}
